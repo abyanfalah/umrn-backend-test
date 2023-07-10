39980914c96871d2cec66d4a38e6b730d6da6010
abyanfalah <abyanf410@gmail.com>
2023-07-10 14:42:13 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::get('/', function () {
 
 Route::prefix('/posts')->group(function () {
     Route::get('/', [PostController::class, 'index']);
+    Route::post('/', [PostController::class, 'store']);
     Route::get('/{post}', [PostController::class, 'show']);
     Route::put('/{post}', [PostController::class, 'update']);
     Route::delete('/{post}', [PostController::class, 'destroy']);
